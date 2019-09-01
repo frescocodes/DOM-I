cf74fa9a5bae2eedc763ed3a2d57cfb80d6207ed
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-08-31 22:52:20 -0400

    most of site repopulated, including nav, header, main-content. still need footer + contact section.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,41 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// Nav Links
+let navLinks = document.querySelectorAll('nav a')
+navLinks[0].textContent = siteContent.nav["nav-item-1"];
+navLinks[1].textContent = siteContent.nav["nav-item-2"];
+navLinks[2].textContent = siteContent.nav["nav-item-3"];
+navLinks[3].textContent = siteContent.nav["nav-item-4"];
+navLinks[4].textContent = siteContent.nav["nav-item-5"];
+navLinks[5].textContent = siteContent.nav["nav-item-6"];
+
+let ctaImg = document.querySelector('#cta-img')
+ctaImg.src = siteContent["cta"]["img-src"];
+
+let bigHead = document.querySelector('h1');
+bigHead.textContent = siteContent['cta']['h1'];
+
+let button = document.querySelector('button');
+button.textContent = siteContent['cta']['button'];
+
+let h4 = document.querySelectorAll('h4');
+h4[0].textContent = siteContent['main-content']['features-h4']
+h4[1].textContent = siteContent['main-content']['about-h4']
+h4[2].textContent = siteContent['main-content']['services-h4']
+h4[3].textContent = siteContent['main-content']['product-h4']
+h4[4].textContent = siteContent['main-content']['vision-h4']
+
+let h4text = document.querySelectorAll('.text-content p')
+h4text[0].textContent = siteContent['main-content']['features-content']
+h4text[1].textContent = siteContent['main-content']['about-content']
+h4text[2].textContent = siteContent['main-content']['services-content']
+h4text[3].textContent = siteContent['main-content']['product-content']
+h4text[4].textContent = siteContent['main-content']['vision-content']
+
+
+let middleImg = document.querySelector('#middle-img');
+console.log(middleImg);
+middleImg.src = siteContent['main-content']['middle-img-src'];
